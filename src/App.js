c28dae1ec968dbfa9ac66ef8d0a35c3e5490f350
NVMate <denys.miand@nure.ua>
2022-07-22 17:18:10 +0300

    Set up a trial request and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
 import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+import { Routes, Route, Link } from "react-router-dom";
 
-function App() {
-  return <h1>Hello</h1>;
+const Get_Products = gql`
+  query GetProducts {
+    categories {
+      name
+      products {
+        name
+      }
+    }
+  }
+`;
+
+export default function App() {
+  const { loading, error, data } = useQuery(Get_Products);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :</p>;
+  return (
+    <>
+      <Header categories={data.categories} />
+      <Routes>
+        <Route path="/*" element={<p>pip</p>} />
+        {data.categories.map((category) => {
+          return (
+            <Route
+              path={"/" + category.name}
+              key={category.name}
+              element={<p>{category.name}</p>}
+            />
+          );
+        })}
+      </Routes>
+    </>
+  );
 }
 
-export default App;
+function Header({ categories }) {
+  return (
+    <div className="Header">
+      {categories.map((category) => {
+        return (
+          <Link to={category.name}>
+            <button className="">{category.name}</button>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetProducts {
+        categories {
+          name
+          products {
+            name
+          }
+        }
+      }
+    `,
+  })
+  .then((result) => {
+    return console.log(result);
+  });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </ApolloProvider>
 );
